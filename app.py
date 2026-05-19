6611ff3c5ae6e7763fe1f0e1c725631903b94386
Renelara0286 <renelara286@gmail.com>
2026-05-19 18:40:04 -0500

    Update app.py

--- app.py
+++ app.py
@@ -22,3 +22,4 @@ numero_pagos = st.number_input("Ingrese el número pagos anuales:" , value = 12)
 
 cuota = lf.cuota_prestamo(monto, interes,anios,numero_pagos)
 st.write("Su cuota mensual es: ",cuota)
+
